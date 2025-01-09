84bec6e4ea945b5ad861bf55aaa799ca3e986935
dxmari <dxmari007@gmail.com>
2025-01-09 12:09:29 +0530

    changes

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const cors = require('cors');
 const { json } = require('body-parser');
 const fileUpload = require('express-fileupload');
 const zip = require('express-easy-zip');
+const AdmZip = require('adm-zip');
 const tinify = require('tinify')
+const sgMail = require('@sendgrid/mail');
 require('dotenv').config();
 
 tinify.key = process.env.TinyKey;
@@ -13,7 +15,27 @@ const app = express();
 app.use(fileUpload({}));
 app.use(json());
 app.use(cors());
-app.use(zip())
+app.use(zip());
+
+const sendEmail = async (toEmails = [], file) => {
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+  const msg = {
+    to: toEmails, // Change to your recipient
+    from: 'mari@culturemonkey.io', // Change to your verified sender
+    subject: 'Imagify - Your file is ready!!',
+    html: '<strong>Please find the attachment</strong>',
+    attachments: [{
+      content: file.content,
+      filename: file.name
+    }]
+  }
+  try {
+    const data = await sgMail.send(msg)
+    console.log('Email sent', data)
+  } catch (error) {
+    console.error(error)
+  }
+}
 
 //setting view engine to ejs
 app.set("view engine", "ejs");
@@ -29,18 +51,22 @@ app.post('/api/compress', async (req, res) => {
     if (!Array.isArray(req.files.images)) {
       req.files.images = [req.files.images]
     }
+    res.json({
+      url: `/imagify/api/download?name=${fpath}`
+    });
     try {
+      const admzip = new AdmZip();
       const fpath = Date.now();
       const dirPath = path.resolve(__dirname, `outputs/${fpath}`);
       fs.mkdirSync(dirPath);
       for (const key in req.files.images) {
         const file = req.files.images[key];
-        await tinify.fromBuffer(file.data).toFile(`${dirPath}/${file.name}`);
+        // await tinify.fromBuffer(file.data).toFile(`${dirPath}/${file.name}`);
+        admzip.addFile(file.name, file.data);
       }
       console.log('tinify.compressionCount', tinify.compressionCount);
-      res.json({
-        url: `/imagify/api/download?name=${fpath}`
-      })
+      const result = admzip.toBuffer();
+      sendEmail(["mari@effy.co.in"], { name: `${fpath}_files.zip`, content: result });
     } catch (error) {
       console.log('error', error);
       res.status(400).json({
